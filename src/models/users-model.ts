88f3aa98b2e3afbb5b156f387c1c54b357982d1a
KatianaHanisch <katianahanisch@gmail.com>
2024-08-08 16:18:06 -0400

    feat: add users model

---
+++ src/models/users-model.ts
@@ -0,0 +1,37 @@
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
+
+const users = [
+  { id: "1", name: "teste", email: "teste@gmail.com", password: "123" },
+];
+
+const getAllUsers = () => users;
+
+const getUserById = (id: string) => users.find((user) => user.id === id);
+
+const getUserByEmail = (email: string) =>
+  users.find((user) => user.email === email);
+
+const createUser = (name: string, email: string, password: string) => {
+  const userAlredyExists = users.find((user) => user.email === email);
+
+  if (userAlredyExists) return null;
+
+  const newUser = {
+    id: uuidv4(),
+    name,
+    email,
+    password: bcrypt.hashSync(password, 10),
+  };
+
+  users.push(newUser);
+
+  return newUser;
+};
+
+export const usersModel = {
+  getAllUsers,
+  getUserById,
+  getUserByEmail,
+  createUser,
+};
