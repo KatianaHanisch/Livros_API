7237398e2ea98faf1f63c319b6dd2e5474de823d
KatianaHanisch <katianahanisch@gmail.com>
2024-08-09 11:27:48 -0400

    feat: add loan routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 
 import { booksController } from "../controllers/books-controller";
+import { loansController } from "../controllers/loans-controller";
+import { ensureAuth } from "../middlewares/auth-middleware";
 
 export const apiRouter = Router();
 
@@ -10,3 +12,9 @@ apiRouter.get("/books/:id", booksController.getBookById);
 apiRouter.post("/books", booksController.createBook);
 apiRouter.put("/books/:id", booksController.updateBook);
 apiRouter.delete("/books/:id", booksController.deleteBook);
+
+apiRouter.get("/loans", loansController.getAllLoans);
+apiRouter.get("/loans/:id", loansController.getLoanById);
+
+apiRouter.post("/loans", ensureAuth, loansController.createLoan);
+apiRouter.post("/loans/:id/return", loansController.returnLoan);
